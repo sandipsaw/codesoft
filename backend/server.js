8aebc34bf5032f81f61f4d5f8a5d6a5123112dd3
sandip saw <sandipsaw449@gmail.com>
2026-05-11 14:48:11 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['https://talenthub-ebon.vercel.app/','http://localhost:5173'],
     credentials: true
 }));
 
